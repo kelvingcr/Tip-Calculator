f1da1dc332398623bdd9152e30dba4b6e9b8d9ec
Kelvin Rodrigues <jedsonsougato15@gmail.com>
2023-05-17 22:30:31 -0300

    Finalizado

--- app/src/main/java/com/kelvincosta/tiptime/MainActivity.kt
+++ app/src/main/java/com/kelvincosta/tiptime/MainActivity.kt
@@ -1,34 +1,39 @@
 package com.kelvincosta.tiptime
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,7 +65,14 @@ fun TipTimeScreen() {
     var amountInput by remember { mutableStateOf("") }
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+
+    var tipInput by remember { mutableStateOf("") }
+
+    val focusManager = LocalFocusManager.current
+
+    var roundUp by remember { mutableStateOf(false) }
+
+    val tip = calculateTip(amount, tipInput.toDoubleOrNull() ?: 15.0, roundUp)
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -72,10 +84,37 @@ fun TipTimeScreen() {
         )
         Spacer(modifier = Modifier.height(16.dp))
 
-        EditNumberField(amountInput) {
-            amountInput = it
-        }
+        EditNumberField(
+            label = R.string.bill_amount,
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            keyboardOptions=  KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions (
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                }
+            )
+        )
 
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChange = { tipInput = it},
+            keyboardOptions=  KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                }
+            )
+        )
+
+        RoundTheTipRow(roundUp = roundUp, onRoundUpChanged = {roundUp = it})
 
         Spacer(modifier = Modifier.height(24.dp))
         Text(
@@ -89,22 +128,57 @@ fun TipTimeScreen() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditNumberField(value: String, onValueChange: (String) -> Unit) {
+fun EditNumberField(
+    modifier: Modifier = Modifier,
+    @StringRes label: Int,
+    value: String,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
+) {
 
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = value,
         onValueChange = onValueChange,
-        label = { Text(text = stringResource(id = R.string.cost_of_service)) },
+        label = { Text(text = stringResource(id = label)) },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
     )
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0) : String {
-    val tip = tipPercent / 100 * amount
+@Composable
+fun RoundTheTipRow(
+    modifier: Modifier = Modifier,
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+            Text(text = stringResource(id = R.string.round_up_tip))
+            Switch(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End),
+                checked = roundUp,
+                onCheckedChange = onRoundUpChanged,
+            )
+        }
+    }
+
+
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean) : String {
+    var tip = tipPercent / 100 * amount
+
+    if(roundUp) tip = kotlin.math.ceil((tip))
+
     return NumberFormat.getCurrencyInstance().format(tip)
 }
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
